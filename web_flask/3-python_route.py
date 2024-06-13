84795d6684653da2b2319b11a78f61aedeff2d5e
maridadithierry <t.maridadithierry@alustudent.com>
2024-06-13 16:21:01 +0200

    mySQL

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -15,3 +15,22 @@ def hbnb():
     """ function that displays hbnb!"""
     return 'HBNB'
 
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ function that returns "C" followed by the value of the text"""
+    return 'C {}'.format(text.replace('_', ''))
+
+
+@app.route('/pythons', strict_slashes=False)
+@app.route('/pythons/<text>', strict_slashes=False)
+def python_is_cool(text = 'is  cool'):
+    """ function that returns python followed by the value of the text"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
