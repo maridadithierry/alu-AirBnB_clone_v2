1e66bd72e38bf49c0f6f476509404f15c90e2285
maridadithierry <t.maridadithierry@alustudent.com>
2024-06-13 15:14:48 +0200

    changes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,3 +15,12 @@ def hbnb():
     """ function that displays hbnb!"""
     return 'HBNB'
 
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ function that returns "C" followed by the value of the text"""
+    return 'C {}'.format(text.replace('_', ''))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
