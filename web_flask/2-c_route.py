9f0ae23619d6fa5bdf7801460b6f7796035bdae1
maridadithierry <t.maridadithierry@alustudent.com>
2024-06-13 15:36:45 +0200

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-""" Script that runs a flask application"""
-from flask import Flask
-app = Flask(__name__)
+"""Fabric Script that distributes an archives"""
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ function that return " hello hbnb" """
-    return 'Hello HBNB!'
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.227.179.101', '3.90.204.71']
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ function that displays hbnb!"""
-    return 'HBNB'
-
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """ function that returns "C" followed by the value of the text"""
-    return 'C {}'.format(text.replace('_', ''))
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+def do_deploy(archive_path):
+    """Archives to web-servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
